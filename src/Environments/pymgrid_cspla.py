88327a49a3b8e4e17b82a4a219c0230218d3d3ed
bhushan <nagareddy318@gmail.com>
2021-09-07 19:39:34 +0530

    Update pymgrid_cspla.py

--- src/Environments/pymgrid_cspla.py
+++ src/Environments/pymgrid_cspla.py
@@ -19,7 +19,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 """
-from pymgridbhu.Environments.Environment_modified import Environment
+from pymgridbhu.Environments.Environment import Environment
 import numpy as np
 import gym
 from gym.utils import seeding
