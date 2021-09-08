b9843ea2b3b64e5e877b747b05e94baa214af1a9
bhushan <nagareddy318@gmail.com>
2021-09-08 09:51:37 +0530

    modified

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="pymgridbhu",
     package_dir={"": "src"},
     packages=find_packages("src"),
-    version="0.1.0",
+    version="0.0.0",
     python_requires=">=3.0",
     include_package_data=True,
     install_requires=[
